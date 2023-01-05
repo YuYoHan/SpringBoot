2d79ef4bfdb04d92597cf4a649d159324438f88f
YuYoHan <zxzz8014@naver.com>
2023-01-05 21:00:59 +0900

    [Study] add SpringConfig_class code

--- hello-spring/src/main/java/com/example/hellospring/service/SpringConfig.java
+++ hello-spring/src/main/java/com/example/hellospring/service/SpringConfig.java
@@ -1,12 +1,23 @@
 package com.example.hellospring.service;
 
+import com.example.hellospring.repository.JdbcMemberRepository;
 import com.example.hellospring.repository.MemberRepository;
 import com.example.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +26,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepositrory() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
