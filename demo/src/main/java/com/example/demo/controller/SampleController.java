bd9b0fa060c87bf254302693df9c5d0fb981754d
YuYoHan <zxzz8014@naver.com>
2023-01-10 18:45:45 +0900

    [Study] Exception

---
+++ demo/src/main/java/com/example/demo/controller/SampleController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/user/userInfo")
+    public String userInfo() {
+
+        return "/user/userInfo";
+    }
+
+    @GetMapping("/user/userData")
+    public String userData() {
+
+        return "/user/userinfo";
+    }
+}
