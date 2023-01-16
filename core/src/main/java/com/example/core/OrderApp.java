93051a5ea4a48fb89c647a8150e6cdfde0352c8e
YuYoHan <zxzz8014@naver.com>
2023-01-16 10:13:41 +0900

    [Study] Only programming in Java

--- core/src/main/java/com/example/core/OrderApp.java
+++ core/src/main/java/com/example/core/OrderApp.java
@@ -1,5 +1,25 @@
 package com.example.core;
 
+import com.example.core.member.Grade;
+import com.example.core.member.Member;
+import com.example.core.member.MemberService;
+import com.example.core.member.MemberServiceImpl;
+import com.example.core.order.Order;
+import com.example.core.order.OrderService;
+import com.example.core.order.OrderServiceImpl;
+
 public class OrderApp {
-    
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        long memberId = 1;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.creatOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("order.calculatePrice = " + order.calculatePrice());
+    }
 }
