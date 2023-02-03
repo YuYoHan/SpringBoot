3c0ead9ab6eaf850a7f073dd72495299bf2ab966
YuYoHan <zxzz8014@naver.com>
2023-02-03 10:52:04 +0900

    [Study] Spring Boot Guide

--- demo3/src/main/java/com/example/demo3/controller/PostController.java
+++ demo3/src/main/java/com/example/demo3/controller/PostController.java
@@ -1,10 +1,24 @@
 package com.example.demo3.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
 public class PostController {
 
+    @PostMapping(value = "/user")
+    public String postUser(@RequestBody Map<String, Objects> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+        return sb.toString();
+    }
 }
