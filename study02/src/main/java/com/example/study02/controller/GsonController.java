27e839f3a7b27e452f6b0032fe807c7f06499e88
YuYoHan <zxzz8014@naver.com>
2023-02-12 20:08:05 +0900

    [Study] GSON

--- study02/src/main/java/com/example/study02/controller/GsonController.java
+++ study02/src/main/java/com/example/study02/controller/GsonController.java
@@ -1,5 +1,6 @@
 package com.example.study02.controller;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,22 @@ public class GsonController {
         obj.add("data", data);
 
         return obj.toString();
+    }
+
+    @ResponseBody
+    @RequestMapping("/test2")
+    public String test2() {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("title", "Gson Test Tile");
+        jsonObject.addProperty("content", "Gson Test Content");
+
+        JsonArray jsonArray = new JsonArray();
+        for (int i = 0; i < 5; i++) {
+            JsonObject jsonObject2 = new JsonObject();
+            jsonObject2.addProperty("data", i);
+            jsonArray.add(jsonObject2);
+        }
+        jsonObject.add("testData", jsonArray);
+        return jsonObject.toString();
     }
 }
