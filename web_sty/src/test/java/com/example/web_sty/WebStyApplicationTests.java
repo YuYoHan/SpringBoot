62b3fbccc2f3d3fe93509640431f68dd429e911c
YuYoHan <zxzz8014@naver.com>
2023-02-13 12:15:57 +0900

    [Study] setting

---
+++ web_sty/src/test/java/com/example/web_sty/WebStyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_sty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
