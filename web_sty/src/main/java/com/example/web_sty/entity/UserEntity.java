b19388782c535c550d2f8861efe0ec354a7bcb79
YuYoHan <zxzz8014@naver.com>
2023-02-14 22:47:04 +0900

    [Study] test

--- web_sty/src/main/java/com/example/web_sty/entity/UserEntity.java
+++ web_sty/src/main/java/com/example/web_sty/entity/UserEntity.java
@@ -1,4 +1,29 @@
 package com.example.web_sty.entity;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "user")
 public class UserEntity {
+
+    // pk
+    @Id
+    // auto_increment
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String userEmail;
+
+    @Column
+    private String userPw;
+
+    @Column
+    private String userName;
 }
