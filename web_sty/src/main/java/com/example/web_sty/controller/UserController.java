6ae3df6df4fda79ebde61c55fdb87cf31082ced0
YuYoHan <zxzz8014@naver.com>
2023-02-28 19:22:57 +0900

    [Study] ajax

--- web_sty/src/main/java/com/example/web_sty/controller/UserController.java
+++ web_sty/src/main/java/com/example/web_sty/controller/UserController.java
@@ -6,10 +6,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -109,4 +107,10 @@ public class UserController {
         return "redirect:/";
     }
 
+    @PostMapping("/user/email-check")
+    // ajax를 쓸 때는 반드시 @ResponseBody를 써야한다.
+    public @ResponseBody String emailCheck(@RequestParam("userEmail") String userEmail) {
+        log.info("userEmail : " + userEmail);
+        return "체크완료!";
+    }
 }
