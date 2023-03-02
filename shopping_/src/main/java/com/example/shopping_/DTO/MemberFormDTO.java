7bbd69a5518e3a3ea175c845c8586b76a099e22c
YuYoHan <zxzz8014@naver.com>
2023-03-02 09:17:07 +0900

    [Feat] save signup data

---
+++ shopping_/src/main/java/com/example/shopping_/DTO/MemberFormDTO.java
@@ -0,0 +1,13 @@
+package com.example.shopping_.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberFormDTO {
+    private String name;
+    private String email;
+    private String password;
+    private String addr;
+}
