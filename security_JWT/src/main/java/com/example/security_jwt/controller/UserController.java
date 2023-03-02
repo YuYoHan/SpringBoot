30c962a9e87a44318c76a8a6c2788271b6c06269
YuYoHan <zxzz8014@naver.com>
2023-03-02 13:49:15 +0900

    [Add] MemberController

--- security_JWT/src/main/java/com/example/security_jwt/controller/UserController.java
+++ security_JWT/src/main/java/com/example/security_jwt/controller/UserController.java
@@ -21,4 +21,18 @@ public class UserController {
         return "manager";
     }
 
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/joinProc")
+    public String joinProc() {
+        return "login";
+    }
+
 }
--- shopping_/src/main/java/com/example/shopping_/controller/MemberController.java
+++ shopping_/src/main/java/com/example/shopping_/controller/MemberController.java
@@ -1,4 +1,23 @@
 package com.example.shopping_.controller;
 
+import com.example.shopping_.DTO.MemberFormDTO;
+import com.example.shopping_.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/new")
+    private String memberForm(Model model){
+        model.addAttribute("memberFormDTO", new MemberFormDTO());
+        return "member/memberForm";
+    }
 }
