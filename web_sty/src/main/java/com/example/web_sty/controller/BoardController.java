f3d96a5dd5c3175ab391f7c4731225aa59be09c3
YuYoHan <zxzz8014@naver.com>
2023-03-03 11:28:02 +0900

    [Feat] save

--- web_sty/src/main/java/com/example/web_sty/controller/BoardController.java
+++ web_sty/src/main/java/com/example/web_sty/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.example.web_sty.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BoardController {
     @GetMapping("/save")
     public String saveForm() {
-        return "save";
+        return "/board/save";
+    }
+
+    @PostMapping("/save")
+    public String save() {
+        return "/board/save";
     }
 }
