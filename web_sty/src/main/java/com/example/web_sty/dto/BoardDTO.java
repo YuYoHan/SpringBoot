80d77845ad8e6b18d3a08c41fa9e919a00f7b793
YuYoHan <zxzz8014@naver.com>
2023-03-03 14:59:01 +0900

    [Add] BoardDTO

--- web_sty/src/main/java/com/example/web_sty/dto/BoardDTO.java
+++ web_sty/src/main/java/com/example/web_sty/dto/BoardDTO.java
@@ -1,14 +1,14 @@
 package com.example.web_sty.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor      // 기본생성자
+@AllArgsConstructor     // 모든 필드를 매개변수로 하는 생성자
 public class BoardDTO {
     private Long id;
     private String boardWriter;
