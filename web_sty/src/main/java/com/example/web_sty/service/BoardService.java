2fdb17207baf0d51689467c2d2cdea582c72fffb
YuYoHan <zxzz8014@naver.com>
2023-03-03 16:39:31 +0900

    [Docs] BoardService

--- web_sty/src/main/java/com/example/web_sty/service/BoardService.java
+++ web_sty/src/main/java/com/example/web_sty/service/BoardService.java
@@ -1,8 +1,21 @@
 package com.example.web_sty.service;
 
+import com.example.web_sty.dto.BoardDTO;
+import com.example.web_sty.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
+// DTO → Entity (Entity Class)
+// Entity → DTO (DTO Class)
 
+@Service
+@RequiredArgsConstructor
 public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public void save(BoardDTO boardDTO) {
+
+        boardRepository.save();
+    }
 }
