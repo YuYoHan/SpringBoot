db4b7b78cbc4d7016f84cbf7a9260f334584665b
YuYoHan <zxzz8014@naver.com>
2023-03-05 20:26:05 +0900

    [Docs] HelloController

--- rest_book1/src/main/java/com/example/rest_book1/controller/HelloController.java
+++ rest_book1/src/main/java/com/example/rest_book1/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.rest_book1.controller;
 
+import com.example.rest_book1.dto.HelloResponseDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어준다.
@@ -11,5 +13,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDTO helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDTO(name, amount);
     }
 }
