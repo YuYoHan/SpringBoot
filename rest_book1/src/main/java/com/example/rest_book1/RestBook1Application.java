f3bc3d08f32283a8e8cb8b99ec68b558d0747402
YuYoHan <zxzz8014@naver.com>
2023-03-05 18:29:20 +0900

    [Add] HelloController

---
+++ rest_book1/src/main/java/com/example/rest_book1/RestBook1Application.java
@@ -0,0 +1,13 @@
+package com.example.rest_book1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBook1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestBook1Application.class, args);
+    }
+
+}
---
+++ rest_book1/src/test/java/com/example/rest_book1/RestBook1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_book1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBook1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
