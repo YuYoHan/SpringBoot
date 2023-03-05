3b4cf332ddc39ce17e72ebbd57ba8aec40fdfe55
YuYoHan <zxzz8014@naver.com>
2023-03-05 18:40:37 +0900

    [Add] HelloController

--- rest/src/main/java/com/example/rest/controller/HelloController.java
+++ rest/src/main/java/com/example/rest/controller/HelloController.java
@@ -1,8 +1,14 @@
 package com.example.rest.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    // GET
+    // /hello-world
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
 }
