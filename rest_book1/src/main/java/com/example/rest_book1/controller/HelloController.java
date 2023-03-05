917cfc8eeff24a2950c8940eb8b81ff363f209e7
YuYoHan <zxzz8014@naver.com>
2023-03-05 18:46:08 +0900

    [Add] HelloController

---
+++ rest_book1/src/main/java/com/example/rest_book1/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.rest_book1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어준다.
+// 메소드마다 @ResponseBody를 붙여도 되지만
+// 클래스에 @RestController를 쓰면 한번에 해결할 수 있다.
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+}
