1dd6967811f9da2204f2f401f6923b28aa969567
YuYoHan <zxzz8014@naver.com>
2023-03-06 18:27:05 +0900

    [Docs] PostsApiController

--- rest_book1/src/main/java/com/example/rest_book1/controller/PostsApiController.java
+++ rest_book1/src/main/java/com/example/rest_book1/controller/PostsApiController.java
@@ -1,4 +1,20 @@
 package com.example.rest_book1.controller;
 
+import com.example.rest_book1.dto.PostsSaveRequestsDTO;
+import com.example.rest_book1.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestsDTO postsSaveRequestsDTO) {
+        return postsService.save(postsSaveRequestsDTO);
+    }
 }
