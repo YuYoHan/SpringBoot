2b687ba4698287c54fa8d1907863056052423677
YuYoHan <zxzz8014@naver.com>
2023-03-06 18:31:38 +0900

    [Docs] PostsService

--- rest_book1/src/main/java/com/example/rest_book1/service/PostsService.java
+++ rest_book1/src/main/java/com/example/rest_book1/service/PostsService.java
@@ -1,4 +1,18 @@
 package com.example.rest_book1.service;
 
+import com.example.rest_book1.domain.PostsRepository;
+import com.example.rest_book1.dto.PostsSaveRequestsDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestsDTO postsSaveRequestsDTO) {
+        return postsRepository.save(postsSaveRequestsDTO.toEntity()).getId();
+    }
 }
