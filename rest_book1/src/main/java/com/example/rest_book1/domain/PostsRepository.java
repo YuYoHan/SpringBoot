f65bda072cc5a270616473b7d6dbd6e51575b5d1
YuYoHan <zxzz8014@naver.com>
2023-03-06 15:13:03 +0900

    [Test] PostsRepositoryTest

--- rest_book1/src/main/java/com/example/rest_book1/domain/PostsRepository.java
+++ rest_book1/src/main/java/com/example/rest_book1/domain/PostsRepository.java
@@ -1,4 +1,4 @@
-package com.example.rest_book1.repository;
+package com.example.rest_book1.domain;
 
 import com.example.rest_book1.domain.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
---
+++ rest_book1/src/test/java/com/example/rest_book1/domain/PostsRepositoryTest.java
@@ -0,0 +1,53 @@
+package com.example.rest_book1.domain;
+
+import org.aspectj.lang.annotation.After;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterAll
+    public void cleanUp() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("게시글 저장 불러오기")
+    public void saveBoardTest() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        // 테이블에 posts에 insert/update 쿼리를 실행합니다.
+        // id 값이 있다면 update, 없다면 insert 쿼리가 실행됩니다.
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("tester@naver.com")
+                .build());
+
+        // when
+        // 테이블 posts에 있는 모든 데이터를 조회해오는 메소드입니다.
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
