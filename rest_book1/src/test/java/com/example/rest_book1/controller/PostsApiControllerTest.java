c911309ebaa37d1beeee589e08f86cc75908cef8
YuYoHan <zxzz8014@naver.com>
2023-03-07 16:42:29 +0900

    [Test] PostsApiControllerTest

--- rest_book1/src/test/java/com/example/rest_book1/controller/PostsApiControllerTest.java
+++ rest_book1/src/test/java/com/example/rest_book1/controller/PostsApiControllerTest.java
@@ -3,14 +3,19 @@ package com.example.rest_book1.controller;
 import com.example.rest_book1.domain.Posts;
 import com.example.rest_book1.domain.PostsRepository;
 import com.example.rest_book1.dto.PostsSaveRequestsDTO;
+import com.example.rest_book1.dto.PostsUpdateRequestDTO;
+import lombok.extern.log4j.Log4j2;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -21,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Log4j2
 class PostsApiControllerTest {
 
     @LocalServerPort
@@ -38,7 +44,8 @@ class PostsApiControllerTest {
     }
 
     @Test
-    public void Posts_등록된다() throws Exception {
+    @DisplayName("Posts_등록된다 테스트")
+    public void test1() throws Exception {
         // given
         String title = "title";
         String content = "content";
@@ -61,6 +68,46 @@ class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         Assertions.assertThat(all.get(0).getTitle()).isEqualTo(title);
         Assertions.assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    @DisplayName("Posts_수정된다 테스트")
+    public void test2() {
+        // given
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                        .title("title")
+                        .content("content")
+                        .author("author")
+                .build());
+
+        Long updateId = savedPosts.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDTO requestDTO = PostsUpdateRequestDTO.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        // HttpEntity클래스는 HTTP요청또는 응답에 해당하는 HttpHeader와 HttpBody를 포함하는 클래스다.
+        HttpEntity<PostsUpdateRequestDTO> requestEntity = new HttpEntity<>(requestDTO);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(
+                url, HttpMethod.PUT, requestEntity, Long.class);
+
+        // then
+        Assertions.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        log.info("-----------------------------------");
+        log.info("수정확인 : " + all);
+        Assertions.assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        Assertions.assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+
     }
 
 
