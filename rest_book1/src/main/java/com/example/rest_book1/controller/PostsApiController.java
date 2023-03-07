79b1310d6928c8bbd7ad1e4f0dc1992112967727
YuYoHan <zxzz8014@naver.com>
2023-03-07 14:12:06 +0900

    [Feat] update

--- rest_book1/src/main/java/com/example/rest_book1/controller/PostsApiController.java
+++ rest_book1/src/main/java/com/example/rest_book1/controller/PostsApiController.java
@@ -3,9 +3,7 @@ package com.example.rest_book1.controller;
 import com.example.rest_book1.dto.PostsSaveRequestsDTO;
 import com.example.rest_book1.service.PostsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +14,10 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestsDTO postsSaveRequestsDTO) {
         return postsService.save(postsSaveRequestsDTO);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsSaveRequestsDTO requestsDTO) {
+        return postsService.update(id,requestsDTO);
     }
 }
