4782f0415ef9a3ec9460b356b9c7329edc10b92b
YuYoHan <zxzz8014@naver.com>
2023-03-08 11:09:31 +0900

    [Add] UserController

---
+++ rest/src/main/java/com/example/rest/user/UserController.java
@@ -0,0 +1,10 @@
+package com.example.rest.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private  UserDaoService service;
+}
