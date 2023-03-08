bac7328fed6b7199a5261fa24f89a92706b6d0e7
YuYoHan <zxzz8014@naver.com>
2023-03-08 09:54:45 +0900

    [Add] User

---
+++ rest/src/main/java/com/example/rest/user/User.java
@@ -0,0 +1,14 @@
+package com.example.rest.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    private LocalDateTime joinDate;
+}
