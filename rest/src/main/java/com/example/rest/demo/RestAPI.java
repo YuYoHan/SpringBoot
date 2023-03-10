c502941803e2c2c81f23a22591ebfa6ea72997ce
YuYoHan <zxzz8014@naver.com>
2023-03-10 15:23:18 +0900

    [Study] REST 공부중

---
+++ rest/src/main/java/com/example/rest/demo/RestAPI.java
@@ -0,0 +1,27 @@
+package com.example.rest.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class RestAPI {
+
+    @GetMapping("")
+    public void GetAll() {
+
+    }
+
+    @PostMapping("")
+    public void Add() {
+
+    }
+
+    @PutMapping("")
+    public void Update() {
+
+    }
+
+    @DeleteMapping("")
+    public void del() {
+
+    }
+}
