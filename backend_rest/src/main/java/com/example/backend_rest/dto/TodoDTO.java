78885242aa3e1d53aced69efeb9744ee6499ccea
YuYoHan <zxzz8014@naver.com>
2023-03-11 17:59:44 +0900

    [Docs] TodoDTO

--- backend_rest/src/main/java/com/example/backend_rest/dto/TodoDTO.java
+++ backend_rest/src/main/java/com/example/backend_rest/dto/TodoDTO.java
@@ -1,5 +1,7 @@
 package com.example.backend_rest.dto;
 
+import com.example.backend_rest.model.TodoEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -9,6 +11,21 @@ import lombok.ToString;
 @ToString
 public class TodoDTO {
     private String id;
-    private String userId;
+    private String title;
     private boolean done;
+
+    @Builder
+
+    public TodoDTO(final TodoEntity todoEntity) {
+        this.id = todoEntity.getId();
+        this.title = todoEntity.getTitle();
+        // isDone인 이유는 set은 setDone인데 get은 isDone이다.
+        this.done = todoEntity.isDone();
+    }
+
+    /*
+    *
+    *
+    *
+    * */
 }
