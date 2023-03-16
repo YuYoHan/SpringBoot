538de7bda1423b57a54e28b7cbad3c4d87f7ea83
YuYoHan <zxzz8014@naver.com>
2023-03-16 21:00:59 +0900

    [Add] TodoRepository에 쿼리추가
    findByUserId 메서드를 생성해주고 @Query로만드는법을 주석처리를
    해놨습니다.

--- backend_rest/src/main/java/com/example/backend_rest/persistence/TodoRepository.java
+++ backend_rest/src/main/java/com/example/backend_rest/persistence/TodoRepository.java
@@ -2,6 +2,14 @@ package com.example.backend_rest.persistence;
 
 import com.example.backend_rest.model.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TodoRepository extends JpaRepository<TodoEntity, String> {
+    List<TodoEntity> findByUserId(String userId);
+
+    // ?1은 메서드의 매개변수의 순서 위치이다.
+//    @Query("select * from TodoEntity t where t.userId = ?1")
+//    List<TodoEntity> findByUserIdQuery(String userId);
 }
