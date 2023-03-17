0ad380105f87c9bc57fbe8bcf6069bac14861c6c
YuYoHan <zxzz8014@naver.com>
2023-03-17 22:46:35 +0900

    [Study] App.js

--- backend_rest/react-workspace/todo-react-app/src/Todo.js
+++ backend_rest/react-workspace/todo-react-app/src/Todo.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Todo = () => {
+const Todo = (props) => {
+    // useState는 리액트의 훅중 하나이며 함수형 컴포넌트에서 상태 변수를 사용할 수 있도록 해줌
+    // 훅을 이용하면 리액트가 제공하는 기능과 상태변수를 사용할 수 있다.
+    // 더 구체적으로 말하면 리액트가 제공하는 useState와 같은 일련의 함수들을 훅이라고 한다.
+    /*
+        상태변수 :
+            시간이 지남에 따라 또 컴포넌트의 사용자가 컴포넌트와 상호작용하는 동안 변경되는 변수
+    */
+    const [item, setItem] = useState(props.item);
+
     return (
         <div className="Todo">
-            <input type="checkbox" id="todo0" name="todo0" value="todo0" />
-            <label for="todo0">Todo 컴포넌트 만들기</label>
+            <input
+                type="checkbox"
+                id={item.id}
+                name={item.id}
+                value={item.done}
+            />
+            <label for={item.id}>{item.title}</label>
         </div>
     );
 };
