16d5496b2caa58dc63c625485ec256fd802880af
YuYoHan <zxzz8014@naver.com>
2023-03-18 10:09:39 +0900

    [Feat] enterkeyHandler 함수 생성
    함수를 만들고 onKeyPress로 TextField에 연결했습니다.
    이제 키보드의 키 이벤트 발생 시 항상 실행됩니다.

--- backend_rest/react-workspace/todo-react-app/src/AddTodo.js
+++ backend_rest/react-workspace/todo-react-app/src/AddTodo.js
@@ -12,6 +12,13 @@ const AddTodo = (props) => {
         setItem({ title: "" });
     };
 
+    // enterKeyEventHandler 함수
+    const enterKeyEventHandler = (e) => {
+        if (e.key === "Enter") {
+            onButtonClick();
+        }
+    };
+
     // onInputChange 함수 작성
     const onInputChange = (e) => {
         setItem({ title: e.target.value });
@@ -26,6 +33,8 @@ const AddTodo = (props) => {
                     placeholder="Add Todo here"
                     fullWidth
                     onChange={onInputChange}
+                    /*onKeyPress ; 키보드의 키를 누를때마다 실행되는 이벤트 핸들러 */
+                    onKeyPress={enterKeyEventHandler}
                     value={item.title}
                 />
             </Grid>
