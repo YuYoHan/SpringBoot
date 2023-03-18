40e20b90415576652532890accf1f0f2e83ecae5
YuYoHan <zxzz8014@naver.com>
2023-03-18 09:17:20 +0900

    [Feat] AddItem 기능생성

--- backend_rest/react-workspace/todo-react-app/src/AddTodo.js
+++ backend_rest/react-workspace/todo-react-app/src/AddTodo.js
@@ -3,12 +3,24 @@ import { Button, Grid, TextField } from "@mui/material";
 
 const AddTodo = (props) => {
     // 사용자의 입력을 저장할 오브젝트
-    const [ListItem, setItem] = useState({ title: "" });
+    const [item, setItem] = useState({ title: "" });
 
+    // onInputChange 함수 작성
+    const onInputChange = (e) => {
+        setItem({ title: e.target.value });
+        console.log(item);
+    };
+
+    // onInputChange 함수 TextField에 연결
     return (
         <Grid container style={{ marginTop: 20 }}>
             <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-                <TextField placeholder="Add Todo here" fullWidth />
+                <TextField
+                    placeholder="Add Todo here"
+                    fullWidth
+                    onChange={onInputChange}
+                    value={item.title}
+                />
             </Grid>
             <Grid xs={1} me={1} item>
                 <Button
