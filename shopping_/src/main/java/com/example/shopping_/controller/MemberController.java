5b53053633fa4937d24ed480f4a0968fb9f79dca
YuYoHan <zxzz8014@naver.com>
2023-03-18 15:45:39 +0900

    [Add] memberFrom 메서드

--- shopping_/src/main/java/com/example/shopping_/controller/MemberController.java
+++ shopping_/src/main/java/com/example/shopping_/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.example.shopping_.controller;
 
 import com.example.shopping_.DTO.MemberFormDTO;
+import com.example.shopping_.entity.Member;
 import com.example.shopping_.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,10 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/new")
     private String memberForm(Model model){
         model.addAttribute("memberFormDTO", new MemberFormDTO());
         return "member/memberForm";
+    }
+
+    @PostMapping("/new")
+    public String memberForm(MemberFormDTO memberFormDTO) {
+        Member member = Member.createMember(memberFormDTO, passwordEncoder);
+        memberService.saveMember(member);
+
+        return "redirect:/";
     }
 }
