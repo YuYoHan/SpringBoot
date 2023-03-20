7e91d11c386d9fe40cbca18c8a5f3c310d3ce843
YuYoHan <zxzz8014@naver.com>
2023-03-20 20:52:33 +0900

    [Study] rest

--- rest/src/main/java/com/example/rest/helloworld/HelloController.java
+++ rest/src/main/java/com/example/rest/helloworld/HelloController.java
@@ -1,11 +1,20 @@
 package com.example.rest.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     // GET
     // /hello-world
     // 예전 : @RequestMapping → 요즘 : @GetMapping
@@ -34,5 +43,10 @@ public class HelloController {
     @GetMapping("/hello-world-bean/path-variable/{name}")
     public HelloWorldBean helloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false)Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
     }
 }
