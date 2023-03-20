baef780167925f59a6c2e9f133a846f615e65136
YuYoHan <zxzz8014@naver.com>
2023-03-20 19:17:42 +0900

    [Study] rest

--- rest/src/main/java/com/example/rest/user/User.java
+++ rest/src/main/java/com/example/rest/user/User.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 public class User {
     private Integer id;
 
-    @Size(min=2)
+    @Size(min=2, message = "name은 2글자 이상 써주세요")
     private String name;
     @Past
     private LocalDateTime joinDate;
