d5472eca40f15829e0eb32dc7104b4561657d85e
YuYoHan <zxzz8014@naver.com>
2023-03-20 15:46:09 +0900

    [Study] rest

--- rest/src/main/java/com/example/rest/user/UserNotFoundException.java
+++ rest/src/main/java/com/example/rest/user/UserNotFoundException.java
@@ -1,8 +1,13 @@
 package com.example.rest.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// HTTP Status code
 // 2xx → OK
 // 4xx → Client
 // 5xx → Server
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String msg) {
         super(msg);
