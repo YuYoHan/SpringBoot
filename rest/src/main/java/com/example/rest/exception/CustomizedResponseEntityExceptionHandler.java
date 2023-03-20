b553cde5be0f774bbf779feb55b786bcd74bbe8d
YuYoHan <zxzz8014@naver.com>
2023-03-20 19:15:21 +0900

    [Study] rest

--- rest/src/main/java/com/example/rest/exception/CustomizedResponseEntityExceptionHandler.java
+++ rest/src/main/java/com/example/rest/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.example.rest.exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +30,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 new Date(), e.getMessage(), request.getDescription(false));
 
         return  new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(
+                new Date(), "Validation Failed", ex.getBindingResult().toString());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
