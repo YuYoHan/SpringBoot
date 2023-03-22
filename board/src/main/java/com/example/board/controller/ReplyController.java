e0678c44aa5f849dcacc710c4a9c507ca547d82a
YuYoHan <zxzz8014@naver.com>
2023-03-22 22:41:29 +0900

    [Study] board

---
+++ board/src/main/java/com/example/board/controller/ReplyController.java
@@ -0,0 +1,19 @@
+package com.example.board.controller;
+
+import com.example.board.service.ReplyService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/reply/*")
+@RestController
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyController {
+
+    private ReplyService service;
+
+    
+}
