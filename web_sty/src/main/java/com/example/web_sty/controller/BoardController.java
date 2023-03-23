e7fe969d0dd782bb7bf7218dd62d19d0acdf751e
YuYoHan <zxzz8014@naver.com>
2023-03-23 23:25:22 +0900

    [Study] board
    JPA로 게시판 만드는 연습

--- web_sty/src/main/java/com/example/web_sty/controller/BoardController.java
+++ web_sty/src/main/java/com/example/web_sty/controller/BoardController.java
@@ -5,10 +5,13 @@ import com.example.web_sty.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @Log4j2
@@ -29,5 +32,14 @@ public class BoardController {
         log.info("boardDTO : " + boardDTO);
         boardService.save(boardDTO);
         return "/board/save";
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        // DB에서 전체 게시글 데이터를 가져와서 list.html에 보여준다.
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "list";
+
     }
 }
