a573e91178f1f569f42ef574177c6e4ded184a1d
YuYoHan <zxzz8014@naver.com>
2023-03-24 11:33:47 +0900

    [Study] book

---
+++ backend_rest/src/main/java/com/example/backend_rest/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package com.example.backend_rest.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private String token;
+    private String userName;
+    private String password;
+    private String id;
+}
