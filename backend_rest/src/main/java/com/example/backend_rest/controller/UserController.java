5ffabb79392a7365ce0b669fc6cfcec8ef98c1f1
YuYoHan <zxzz8014@naver.com>
2023-03-24 12:37:09 +0900

    [Study] book

--- backend_rest/src/main/java/com/example/backend_rest/controller/UserController.java
+++ backend_rest/src/main/java/com/example/backend_rest/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.example.backend_rest.controller;
 
+import com.example.backend_rest.dto.ResponseDTO;
 import com.example.backend_rest.dto.UserDTO;
 import com.example.backend_rest.model.UserEntity;
 import com.example.backend_rest.service.UserService;
@@ -30,6 +31,37 @@ public class UserController {
                     .username(userDTO.getUserName())
                     .password(userDTO.getPassword())
                     .build();
+            // 서비스를 이용해 레포지토리에 유저 저장
+            UserEntity registeredUser = userService.create(user);
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .id(registeredUser.getId())
+                    .userName(registeredUser.getUsername())
+                    .build();
+            return ResponseEntity.ok().body(responseUserDTO);
+        } catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    @PostMapping("/signIn")
+    public ResponseEntity<?> authenicate(@RequestBody UserDTO userDTO) {
+        UserEntity user = userService.getByCredentials(
+                userDTO.getUserName(),
+                userDTO.getPassword()
+        );
+
+        if(user != null) {
+            final UserDTO responseUserDTO = UserDTO.builder()
+                    .userName(user.getUsername())
+                    .id(user.getId())
+                    .build();
+            return ResponseEntity.ok().body(responseUserDTO);
+        } else  {
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error("Login failed")
+                    .build();
+            return ResponseEntity.badRequest().body(responseDTO);
         }
     }
 }
