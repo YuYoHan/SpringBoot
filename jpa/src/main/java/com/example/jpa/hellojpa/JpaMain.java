291951d90e47a3b9578ffab141d95fe82b29c28e
YuYoHan <zxzz8014@naver.com>
2023-03-27 21:26:20 +0900

    [Study] ORM JPA

---
+++ jpa/src/main/java/com/example/jpa/hellojpa/JpaMain.java
@@ -0,0 +1,15 @@
+package com.example.jpa.hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager entityManager = emf.createEntityManager();
+        // code
+        entityManager.close();
+        emf.close();
+    }
+}
