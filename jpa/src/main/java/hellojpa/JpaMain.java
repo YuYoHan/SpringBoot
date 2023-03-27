02340be1114b22f4617c27c7a70c07b2b619eac7
YuYoHan <zxzz8014@naver.com>
2023-03-27 22:26:10 +0900

    [Study] ORM JPA

--- jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,11 +15,25 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = entityManager.find(Member.class, 1L);
+//            Member findMember = entityManager.find(Member.class, 1L);
 
-            // permit()을 안해도 데이터베이스의 name이 변경되었다.
-            // Hello → HelloJPA
-            findMember.setName("HelloJPA");
+            // JPA입장에서는 코드를 짤때 테이블을 대상으로 짜지않는다.
+            // Member라는 객체를 대상으로 짜는 것이다.
+            List<Member> result = entityManager.createQuery("select m from Member as m", Member.class)
+                    // .setFirstResult(5)
+                    // .setMaxResults(8)
+                    // ↑ 이것들이 limit 5, 8 SQL문과 같은 의미입니다.
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member: result
+                 ) {
+                System.out.println("member.name = " + member.getName());
+            }
+//            // permit()을 안해도 데이터베이스의 name이 변경되었다.
+//            // Hello → HelloJPA
+//            findMember.setName("HelloJPA");
 
             // 삭제
 //            entityManager.remove(findMember);
