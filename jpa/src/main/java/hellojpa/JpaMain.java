ecdb9d533fbde492f99dfceb1170c1f210dad935
YuYoHan <zxzz8014@naver.com>
2023-04-04 15:12:38 +0900

    [Study] api-app

--- jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,28 +15,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = entityManager.find(Member.class, 1L);
+            Member member =new Member();
+            member.setId(100L);
+            member.setName("Hello");
 
-            // JPA입장에서는 코드를 짤때 테이블을 대상으로 짜지않는다.
-            // Member라는 객체를 대상으로 짜는 것이다.
-            List<Member> result = entityManager.createQuery("select m from Member as m", Member.class)
-                    // .setFirstResult(5)
-                    // .setMaxResults(8)
-                    // ↑ 이것들이 limit 5, 8 SQL문과 같은 의미입니다.
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-
-            for (Member member: result
-                 ) {
-                System.out.println("member.name = " + member.getName());
-            }
-//            // permit()을 안해도 데이터베이스의 name이 변경되었다.
-//            // Hello → HelloJPA
-//            findMember.setName("HelloJPA");
-
-            // 삭제
-//            entityManager.remove(findMember);
 
             tx.commit();
         } catch (Exception e) {
--- study_project/board/src/main/java/com/example/board/entity/PopularSearchEntity.java
+++ study_project/board/src/main/java/com/example/board/entity/PopularSearchEntity.java
@@ -1,4 +1,22 @@
 package com.example.board.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "popularSearch")
+@Entity(name = "popularSearch")
 public class PopularSearchEntity {
+    @Id
+    private String popularTerm;
+    private int popularSearchCount;
+
 }
