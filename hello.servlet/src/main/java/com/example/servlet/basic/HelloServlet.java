5658d71675aef275060508f57470d13ae4935b24
YuYoHan <zxzz8014@naver.com>
2023-04-12 21:21:34 +0900

    [Study] Hello Servlet

---
+++ hello.servlet/src/main/java/com/example/servlet/basic/HelloServlet.java
@@ -0,0 +1,26 @@
+package com.example.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        // req = org.apache.catalina.connector.RequestFacade@58abdb91
+        System.out.println("req = " + req);
+        // resp = org.apache.catalina.connector.ResponseFacade@566c2ef4
+        System.out.println("resp = " + resp);
+
+        // 쿼리 파라미터 조회
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+    }
+}
